bd69e163a95574edbe571b8e17adb1dfdfd94b1b
PierreBinet <pbinet@etud.insa-toulouse.fr>
2019-05-07 12:13:01 +0200

    added new files

---
+++ processor/VHDL_proc/Proc.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:11:28 05/07/2019 
+-- Design Name: 
+-- Module Name:    Proc - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Proc is
+end Proc;
+
+architecture Behavioral of Proc is
+
+begin
+
+
+end Behavioral;
+
