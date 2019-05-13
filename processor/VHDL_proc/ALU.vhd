0457f870bbc065aee414efd983e6cf18fdd011d8
PierreBinet <pbinet@etud.insa-toulouse.fr>
2019-05-13 12:51:34 +0200

    added update on BR and finished ALU

--- processor/VHDL_proc/ALU.vhd
+++ processor/VHDL_proc/ALU.vhd
@@ -19,7 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -28,22 +29,46 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-
 entity ALU is
-    Port ( A : in  STD_LOGIC_VECTOR (15 downto 0);
-           B : in  STD_LOGIC_VECTOR (15 downto 0);
-           OP : in  STD_LOGIC_VECTOR (4 downto 0);
-           S : out  STD_LOGIC_VECTOR (15 downto 0);
-           O : out  STD_LOGIC;
-           C : out  STD_LOGIC;
-           Z : out  STD_LOGIC;
-           N : out  STD_LOGIC);
+	 generic(NB : natural := 16);
+    Port ( A : in  STD_LOGIC_VECTOR ((NB-1) downto 0);
+           B : in  STD_LOGIC_VECTOR ((NB-1) downto 0);
+           OP : in  STD_LOGIC_VECTOR (3 downto 0);
+           S : out  STD_LOGIC_VECTOR ((NB-1) downto 0);
+			  C : out  STD_LOGIC;
+			  N : out  STD_LOGIC;
+			  Z : out  STD_LOGIC);
+			  
 end ALU;
 
+ --          O : out  STD_LOGIC; implementer overflow
+ --          
+
 architecture Behavioral of ALU is
-
+	signal Sadd : std_logic_vector(NB downto 0);
+	signal Smult : std_logic_vector((2*NB-1) downto 0);
+	signal Stmp : std_logic_vector(NB-1 downto 0);
 begin
+--operations
+--addition
+	Sadd<=('0'&A)+('0'&B);
+	Smult<=A*B;
+	
+	Stmp<=Sadd((NB-1) downto 0) 	when OP=x"1" else
+		Smult((NB-1) downto 0)	when OP=x"2" else
+		A-B 					when OP=x"3" else
+		(others => '0');
+	
+	S <= Stmp;
+	
+--flag treatment
+	C <= Sadd(NB);
+	
+	N <= Stmp(NB-1);
 
+	Z <= '1' when Stmp = x"0000" else
+		  '0';
+	
 
 end Behavioral;
 
--- processor/VHDL_proc/BR.vhd
+++ processor/VHDL_proc/BR.vhd
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -30,23 +30,40 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity BR is
-    Port ( AFR : in STD_LOGIC_VECTOR (4 downto 0);
-           ASR : in STD_LOGIC_VECTOR (4 downto 0);
-			  AWR : in STD_LOGIC_VECTOR (4 downto 0);
-			  WRI : in STD_LOGIC_VECTOR (7 downto 0);
-           FRO : out STD_LOGIC_VECTOR (7 downto 0);
-           SRO : out STD_LOGIC_VECTOR (7 downto 0));
+	 generic(NB : natural := 16);
+    Port ( AFR : in STD_LOGIC_VECTOR (3 downto 0); --address first register
+           ASR : in STD_LOGIC_VECTOR (3 downto 0); --address second register
+           FRO : out STD_LOGIC_VECTOR ((NB-1) downto 0); --first register OUT
+           SRO : out STD_LOGIC_VECTOR ((NB-1) downto 0); --second register OUT
+			  
+			  AWR : in STD_LOGIC_VECTOR (3 downto 0); --address writing register
+			  WRI : in STD_LOGIC_VECTOR ((NB-1) downto 0); --writing register IN
+			  
+			  MODE : in STD_LOGIC; --1 : write, 0 : read
+			  RST : in STD_LOGIC;
+			  CLK : in STD_LOGIC );
+			  
 end BR;
 
 architecture Behavioral of BR is
-type Tab_type is array(4 downto 0) of STD_LOGIC_VECTOR (7 downto 0);
+type Tab_type is array(3 downto 0) of STD_LOGIC_VECTOR ((NB-1) downto 0);
 signal tab : Tab_type;
 begin
-	FRO<=tab(AFR);
-	SRO<=tab(ASR);
+	FRO<=tab(to_integer(unsigned(AFR)));
+	SRO<=tab(to_integer(unsigned(ASR)));
 	process
-	begin
-		tab(AWR)<=WRI;
+	begin --implementer clock et if avec le mode
+		wait until CLK'event and CLK='1';
+		if RST = '0' then
+			tab <= (others => (others => '0'));
+		elsif MODE = '1' then
+			tab(to_integer(unsigned(AWR)))<=WRI;
+--			if (AWR = AFR) then
+--				FRO<=WRI;
+--			elsif (AWR = ASR) then
+--				SRO<=WRI;
+--				end if;
+		end if;
 	end process;
 end Behavioral;
 
